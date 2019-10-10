95cf75b33e0b5f06e53c882364356a2897cfe594
kmuench <muench.kristin@gmail.com>
2019-10-10 17:45:52 -0400

    Update HUGOnames for bioMart 2.38.0

--- R/addHUGOnames.R
+++ R/addHUGOnames.R
@@ -1,17 +1,17 @@
 #' Add HUGO gene names to ensembl list
 #'
-#' You could just call DESeq() in your code... or you could call this nifty function, which 
+#' You could just call DESeq() in your code... or you could call this nifty function, which
 #' also saves the output to a table AND an R variable for later use
-#' 
+#'
 #' Returns DESeq results object.
-#' 
+#'
 #' @param my_df A data frame where rownames are ensembl genes
 #' @param whichEnsembl which biomart dataset to draw from - e.g. "mmusculus_gene_ensembl"
 #' @param transcript_or_gene "ensembl_gene_id" or "ensembl_transcript_id"
 #' @keywords DESeq
 #' @export
 #' @examples
-#' 
+#'
 #' new_results_df <- addHUGOnames(my_results_df, "mmusculus_gene_ensembl", 'ensembl_transcript_id')
 
 # add hugo genes names to this to make intelligible
@@ -21,7 +21,7 @@ addHUGOnames <- function(my_df, whichEnsembl, transcript_or_gene){
   library( "biomaRt" )
   my_df$ensembl <- sapply( strsplit( rownames(my_df), split="[.]" ), "[", 1 )
   ensembl = useMart( "ensembl", dataset = whichEnsembl )
-  genemap <- getBM( attributes = c("ensembl_gene_id", "entrezgene","external_gene_name"), # c("ensembl_gene_id", "entrezgene","external_gene_name") OR c("ensembl_transcript_id", "ensembl_gene_id", "entrezgene", "hgnc_symbol") 
+  genemap <- getBM( attributes = c("ensembl_gene_id", "entrezgene_id","external_gene_name"), # c("ensembl_gene_id", "entrezgene","external_gene_name") OR c("ensembl_transcript_id", "ensembl_gene_id", "entrezgene", "hgnc_symbol")
                     filters = transcript_or_gene,
                     values = my_df$ensembl,
                     mart = ensembl )
--- R/distPCA.R
+++ R/distPCA.R
@@ -26,41 +26,41 @@ distPCA <- function(cds.pca, condition, colorCondName, PC_A, PC_B, title){
   print(rownames(scores))
 
 
-  # plot of PCs - uncomment for labels
-  p= ggplot(data = as.data.frame(cds.pca),
-            aes_string(x = PC_A, y = PC_B ) ) +
-    geom_point() +
-    geom_text_repel(size = 5, aes(label = rownames(as.data.frame(cds.pca)),
-                                  colour = factor(condition),
-                                  fontface='bold')) +
-    geom_hline(yintercept = 0, colour = "gray65") +
-    geom_vline(xintercept = 0, colour = "gray65") +
-    scale_colour_discrete(name=colorCondName,
-                          l=40) +
-    ggtitle(title)
-  print(p)
+  # # plot of PCs - uncomment for labels
+  # p= ggplot(data = as.data.frame(cds.pca),
+  #           aes_string(x = PC_A, y = PC_B ) ) +
+  #   geom_point() +
+  #   geom_text_repel(size = 5, aes(label = rownames(as.data.frame(cds.pca)),
+  #                                 colour = factor(condition),
+  #                                 fontface='bold')) +
+  #   geom_hline(yintercept = 0, colour = "gray65") +
+  #   geom_vline(xintercept = 0, colour = "gray65") +
+  #   scale_colour_discrete(name=colorCondName,
+  #                         l=40) +
+  #   ggtitle(title)
+  # print(p)
 
   
   
   
-  # # plot of PCs - uncomment for shapes
-  # p= ggplot(data = as.data.frame(cds.pca),
-  #           aes_string(x = PC_A, y = PC_B, group=factor(condition) ) ) +
-  #   geom_hline(yintercept = 0, colour = "gray65") +
-  #   scale_fill_discrete(name = colorCondName) +
-  #   labs(color = colorCondName, shape = colorCondName) +
-  #   geom_vline(xintercept = 0, colour = "gray65") +
-  #   geom_point(aes(color=factor(condition), shape=factor(condition), size = factor(condition)) ) +
-  #   ggtitle(title) +
-  #   theme(axis.text.x = element_text(face="bold", size=14),
-  #         axis.text.y = element_text(face="bold", size=14)) +
-  #   theme_bw() +
-  #   scale_color_manual(values=c("#3288BD", "#D53E4F", "#000000", '#a6cee3', '#1f78b4', '#b2df8a',
-  #                               '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6',
-  #                               '#6a3d9a', '#ffff99', '#b15928', '#8dd3c7', '#d9d9d9', rep('#000000', 100)) ) +
-  #   scale_size_manual(values=c(6,7,rep(6,100))) + # because I want the triangles to be a little bigger but all others can be same size, 6
-  #   scale_shape_manual(values=c(c(16:22, 1:15,23:100 )))
-  # print(p)
+  # plot of PCs - uncomment for shapes
+  p= ggplot(data = as.data.frame(cds.pca),
+            aes_string(x = PC_A, y = PC_B, group=factor(condition) ) ) +
+    geom_hline(yintercept = 0, colour = "gray65") +
+    scale_fill_discrete(name = colorCondName) +
+    labs(color = colorCondName, shape = colorCondName) +
+    geom_vline(xintercept = 0, colour = "gray65") +
+    geom_point(aes(color=factor(condition), shape=factor(condition), size = factor(condition)) ) +
+    ggtitle(title) +
+    theme(axis.text.x = element_text(face="bold", size=14),
+          axis.text.y = element_text(face="bold", size=14)) +
+    theme_bw() +
+    scale_color_manual(values=c("#3288BD", "#D53E4F", "#000000", '#a6cee3', '#1f78b4', '#b2df8a',
+                                '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6',
+                                '#6a3d9a', '#ffff99', '#b15928', '#8dd3c7', '#d9d9d9', rep('#000000', 100)) ) +
+    scale_size_manual(values=c(6,7,rep(6,100))) + # because I want the triangles to be a little bigger but all others can be same size, 6
+    scale_shape_manual(values=c(c(16:22, 1:15,23:100 )))
+  print(p)
 
   # save plot
   ggsave(file=paste0(title,"_",colorCondName,'_',PC_A,'_',PC_B,".svg"), plot=p, width=12, height=10)
